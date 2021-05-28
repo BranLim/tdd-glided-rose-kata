ef0106cbb5f08d3249648e8505347465f306a440
Brandon Lim <brandon.lim@zuhlke.com>
2021-05-28 18:35:46 +0800

    Refactor codes for readability and cleanliness

--- src/test/java/com/layhilltech/glidedrose/GlidedRoseTest.java
+++ src/test/java/com/layhilltech/glidedrose/GlidedRoseTest.java
@@ -1,5 +1,6 @@
 package com.layhilltech.glidedrose;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -10,11 +11,19 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GlidedRoseTest {
 
-    @Test
-    void glidedRoseAddNewNormalItemNamedFoo() {
+    private List<Item> items;
+    private GlidedRose glidedRose;
 
-        List<Item> items = List.of(new Item("Foo", 5, 1));
-        GlidedRose glidedRose = new GlidedRose(items);
+    @BeforeEach
+    void setUp() {
+        items = new ArrayList();
+        glidedRose = new GlidedRose(items);
+    }
+
+    @Test
+    void newNormalItemNamedFoo() {
+
+        items.add(new Item("Foo", 5, 1));
         assertEquals("Foo", glidedRose.stocks().get(0).getName());
     }
 
@@ -25,50 +34,45 @@ public class GlidedRoseTest {
 
     @Test
     void normalItemQualityStartAt5ShouldReduceTo4AfterOneDay() {
-        List<Item> items = new ArrayList();
+
         items.add(new Item("Foo", 5, 5));
 
-        GlidedRose glidedRose = new GlidedRose(items);
         glidedRose.updateQuality();
         assertEquals(4, glidedRose.stocks().get(0).getQuality());
     }
 
     @Test
     void normalItemQualityStartAt4ShouldReduceTo2AfterExpiry() {
-        List<Item> items = new ArrayList();
+
         items.add(new Item("Foo", 0, 4));
 
-        GlidedRose glidedRose = new GlidedRose(items);
         glidedRose.updateQuality();
         assertEquals(2, glidedRose.stocks().get(0).getQuality());
     }
 
     @Test
     void normalItemQualityStartAt1ShouldBe0AfterExpiry() {
-        List<Item> items = new ArrayList();
+
         items.add(new Item("Foo", 0, 1));
 
-        GlidedRose glidedRose = new GlidedRose(items);
         glidedRose.updateQuality();
         assertEquals(0, glidedRose.stocks().get(0).getQuality());
     }
 
     @Test
     void agedBrieQualityAt2ShouldIncreaseBy1AfterExpiry() {
-        List<Item> items = new ArrayList();
+
         items.add(new AgedBrieItem("AgedBrie", 0, 2));
 
-        GlidedRose glidedRose = new GlidedRose(items);
         glidedRose.updateQuality();
         assertEquals(3, glidedRose.stocks().get(0).getQuality());
     }
 
     @Test
     void backstagePassesItemQualityStartAt5ShouldIncreaseBy1IfSellInDateIs11Days() {
-        List<Item> items = new ArrayList();
+
         items.add(new BackstagePassItem("BackstagePass", 11, 5));
 
-        GlidedRose glidedRose = new GlidedRose(items);
         glidedRose.updateQuality();
         assertEquals(6, glidedRose.stocks().get(0).getQuality());
     }
@@ -76,80 +80,72 @@ public class GlidedRoseTest {
 
     @Test
     void backstagePassesItemQualityStartAt5ShouldIncreaseBy2IfSellInDateIs10Days() {
-        List<Item> items = new ArrayList();
+
         items.add(new BackstagePassItem("BackstagePass", 10, 5));
 
-        GlidedRose glidedRose = new GlidedRose(items);
         glidedRose.updateQuality();
         assertEquals(7, glidedRose.stocks().get(0).getQuality());
     }
 
     @Test
     void backstagePassesItemQualityStartAt5ShouldIncreaseBy2IfSellInDateIs9Days() {
-        List<Item> items = new ArrayList();
+
         items.add(new BackstagePassItem("BackstagePass", 9, 5));
 
-        GlidedRose glidedRose = new GlidedRose(items);
         glidedRose.updateQuality();
         assertEquals(7, glidedRose.stocks().get(0).getQuality());
     }
 
     @Test
     void backstagePassesItemQualityStartAt5ShouldIncreaseBy3IfSellInDateIs5Days() {
-        List<Item> items = new ArrayList();
+
         items.add(new BackstagePassItem("BackstagePass", 5, 5));
 
-        GlidedRose glidedRose = new GlidedRose(items);
         glidedRose.updateQuality();
         assertEquals(8, glidedRose.stocks().get(0).getQuality());
     }
 
     @Test
     void backstagePassesItemQualityStartAt5ShouldDropTo0IfSellInDateIs0Days() {
-        List<Item> items = new ArrayList();
+
         items.add(new BackstagePassItem("BackstagePass", 0, 5));
 
-        GlidedRose glidedRose = new GlidedRose(items);
         glidedRose.updateQuality();
         assertEquals(0, glidedRose.stocks().get(0).getQuality());
     }
 
     @Test
     void sulfurasItemQualityStartAt80ButNeverChange() {
-        List<Item> items = new ArrayList();
+
         items.add(new SulfurasItem("Sulfuras", 0, 80));
 
-        GlidedRose glidedRose = new GlidedRose(items);
         glidedRose.updateQuality();
         assertEquals(80, glidedRose.stocks().get(0).getQuality());
     }
 
     @Test
     void conjuredItemsWithQualityStartAt10ButReducesTo8After1Day() {
-        List<Item> items = new ArrayList();
+
         items.add(new ConjuredItem("Conjured", 2, 10));
 
-        GlidedRose glidedRose = new GlidedRose(items);
         glidedRose.updateQuality();
         assertEquals(8, glidedRose.stocks().get(0).getQuality());
     }
 
     @Test
     void conjuredItemsWithQualityStartAt5ButReducesTo3After1Day() {
-        List<Item> items = new ArrayList();
+
         items.add(new ConjuredItem("Conjured", 2, 5));
 
-        GlidedRose glidedRose = new GlidedRose(items);
         glidedRose.updateQuality();
         assertEquals(3, glidedRose.stocks().get(0).getQuality());
     }
 
     @Test
     void normalItemWithSellIn4AndDaysQualityStartAt4ShouldDropTo0After5Days() {
-        List<Item> items = new ArrayList();
+
         items.add(new Item("Foo", 4, 4));
 
-        GlidedRose glidedRose = new GlidedRose(items);
         glidedRose.updateQuality();
         glidedRose.updateQuality();
         glidedRose.updateQuality();
